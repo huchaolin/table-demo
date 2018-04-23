c6ec2380dddd790c2f052ad8ccbb5f6d67b9cd3d
OwenHu <715421045@qq.com>
2018-04-23 23:46:54 +0800

    完成合并以及排序

--- src/App.js
+++ src/App.js
@@ -8,17 +8,13 @@ const renderContent =  (value, row, index) => {
     children: value,
     props: {},
   };
-  console.log(value, row ,index)
-  // if (index === 2) {
-  //   obj.props.rowSpan = 2;
-  // }
-  // // These two are merged into above cell
-  // if (index === 3) {
-  //   obj.props.rowSpan = 0;
-  // }
-  // if (index === 4) {
-  //   obj.props.colSpan = 0;
-  // }
+  if (index === 0) {
+    obj.props.rowSpan = row.RouteNameRowSpan;
+  }
+  // These two are merged into above cell
+  if (index <= row.RouteNameRowSpan) {
+    obj.props.rowSpan = 0;
+  }
   return obj;
 }
 
@@ -30,23 +26,72 @@ class App extends Component {
           title: '路线名称',
           dataIndex: 'RouteName',
           key: 'RouteName',
-          render: renderContent
+          render: (value, row, index) => {
+            const obj = {
+              children: value,
+              props: {
+                rowSpan: row.RouteNameRowSpan || 0
+              },
+            };
+
+            return obj;
+          }
         }, {
           title: '班次',
           dataIndex: 'Time',
           key: 'Time',
+          render: (value, row, index) => {
+            const obj = {
+              children: value,
+              props: {
+                rowSpan: row.TimeRowSpan || 0
+              },
+            };
+
+            return obj;
+          }
         }, {
           title: '总人数',
           dataIndex: 'total',
           key: 'total',
+          render: (value, row, index) => {
+            const obj = {
+              children: value,
+              props: {
+                rowSpan: row.TimeRowSpan || 0
+              },
+            };
+
+            return obj;
+          }
         }, {
           title: '航站楼',
           dataIndex: 'Terminal',
           key: 'Terminal',
+          render: (value, row, index) => {
+            const obj = {
+              children: value,
+              props: {
+                rowSpan: row.TerminalRowSpan || 0
+              },
+            };
+
+            return obj;
+          }
         }, {
           title: '航站楼总人数',
           dataIndex: 'terminalTotal',
           key: 'terminalTotal',
+          render: (value, row, index) => {
+            const obj = {
+              children: value,
+              props: {
+                rowSpan: row.TerminalRowSpan || 0
+              },
+            };
+
+            return obj;
+          }
         }, {
           title: '预订人姓名',
           dataIndex: 'Name',
@@ -67,61 +112,68 @@ class App extends Component {
         dataSource:[]
       }
   }
-  handleData(data) {
-    const routeNameObj = {};
-   data.forEach(v => {
-    routeNameObj[v.RouteName] =  routeNameObj[v.RouteName] ? [v, ...routeNameObj[v.RouteName]] : [v];
-  });
-  //计算相同班次的人数
-
-  Object.keys(routeNameObj).forEach(v => {
-    const timeDic = {};
-    routeNameObj[v].forEach(v => {
-        timeDic[v.Time] =  timeDic[v.Time] ? [v, ...timeDic[v.Time]] : [v];
+  createBook(arr, v, obj, key) {
+    arr[v].forEach(v => {
+      obj[v[key]] =  obj[v[key]] ? [...obj[v[key]], v] : [v];
     });
-    //计算相同班次的相同时间总人数
-    Object.keys(timeDic).forEach( v => {
+  }
+  addTotalNum(book, key) {
+    Object.keys(book).forEach( v => {
       let total = 0;
-      timeDic[v].forEach(v => {
-        total = total +v.Number;
+      book[v].forEach(v => {
+        total = total + v.Number;
       });
-      timeDic[v].forEach( v =>{v.total = total});
+      book[v].forEach( v =>{v[key] = total});
     });
+  }
+  addRowSpan(book, key) {
+    Object.keys(book).forEach( v => {
+      let total = 0;
+      book[v][0][key] = book[v].length;
+    });
+  }
+  handleData(data) {
+    const routeNameDic = {};
+    data.forEach(v => {
+      routeNameDic[v.RouteName] =  routeNameDic[v.RouteName] ? [v, ...routeNameDic[v.RouteName]] : [v];
+    });
+  
+    //排序航站楼
+    Object.keys(routeNameDic).forEach(v => {
+      routeNameDic[v].sort( (v1, v2) => {
+       const num1= parseInt(v1.Terminal); 
+       const num2 = parseInt(v2.Terminal); 
+         return num1 - num2;
+      })
+    });
+    //排序时间
+     Object.keys(routeNameDic).forEach(v => {
+      routeNameDic[v].sort( (v1, v2) => {
+       const time1 = new Date(v1.Date +" " + v1.Time); 
+       const time2 = new Date(v2.Date +" " + v2.Time);
+         return time1.getTime() - time2.getTime();
+      })
+    this.addRowSpan(routeNameDic, 'RouteNameRowSpan');
+  //计算相同班次的人数
+  Object.keys(routeNameDic).forEach(v => {
+    const timeDic = {};
+    this.createBook(routeNameDic, v, timeDic, 'Time');
+    //计算相同班次的相同时间总人数
+    this.addTotalNum(timeDic, 'total');
+    this.addRowSpan(timeDic, 'TimeRowSpan');
     //计算相同班次相同时间相同航站楼的总人数
     Object.keys(timeDic).forEach( v => {
       const terminalDic = {};
-      timeDic[v].forEach(v => {
-        terminalDic[v.Terminal] =  terminalDic[v.Terminal] ? [v, ...terminalDic[v.Terminal]] : [v];
-      });
-      Object.keys(terminalDic).forEach( v => {
-        let total = 0;
-        terminalDic[v].forEach(v => {
-          total = total +v.Number;
-        });
-        terminalDic[v].forEach( v =>{v.terminalTotal = total});
-      });
+      this.createBook(timeDic, v, terminalDic, 'Terminal' );
+      this.addTotalNum(terminalDic, 'terminalTotal');
+      this.addRowSpan(terminalDic, 'TerminalRowSpan');
     });
   
   });
-  //排序航站楼
-  Object.keys(routeNameObj).forEach(v => {
-    routeNameObj[v].sort( (v1, v2) => {
-     const num1= parseInt(v1.Terminal); 
-     const num2 = parseInt(v2.Terminal); 
-       return num1 - num2;
-    })
-  });
-  //排序时间
-   Object.keys(routeNameObj).forEach(v => {
-    routeNameObj[v].sort( (v1, v2) => {
-     const time1 = new Date(v1.Date +" " + v1.Time); 
-     const time2 = new Date(v2.Date +" " + v2.Time);
-       return time1.getTime() - time2.getTime();
-    })
   });
    let arr =[];
-   Object.keys(routeNameObj).forEach( v => {
-     arr = arr.concat(routeNameObj[v]);
+   Object.keys(routeNameDic).forEach( v => {
+     arr = arr.concat(routeNameDic[v]);
    });
 
    return arr;
